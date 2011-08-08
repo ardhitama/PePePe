abae945667282f7a17be816913a432a9f28c984a
Mulia Arifandy Nasution <mul14.net@gmail.com>
2011-08-09 00:55:13 +0700

    Tambah struktur database MySQL

---
+++ pepepe.org/schema/mysql.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `username` varchar(32) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `email` varchar(128) NOT NULL,
+  `password` char(40) NOT NULL,
+  `birthday` date NOT NULL,
+  `created_at` datetime NOT NULL,
+  `updated_at` datetime NOT NULL,
+  `is_deleted` tinyint(1) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
