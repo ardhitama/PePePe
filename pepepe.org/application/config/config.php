dc8a69bdd5b343a7043218e24eec13aa0bbdb70a
Mulia Arifandy Nasution <mul14.net@gmail.com>
2011-08-09 21:10:01 +0700

    Ubah base_url agar lebih universal

--- pepepe.org/application/config/config.php
+++ pepepe.org/application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://pepepe.org';
+$config['base_url']	= $_SERVER['SERVER_NAME'] . '/';
 
 /*
 |--------------------------------------------------------------------------
